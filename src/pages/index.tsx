40099ba16311e877d6150bc6fafec27668e19542
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-06-20 13:04:59 +0100

    Install bootstrap and fix mdx dependency with npmrc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import "../scss/site.scss"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
+      <h1>***REMOVED***</h1>
     </main>
   )
 }
