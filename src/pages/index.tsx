deb9e2f9042621ac70fedd9340f06c5f3712701e
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-08-21 18:43:13 +0100

    Put it all in sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ import { formatDate } from "../utils/dateUtils"
 
 const IndexPage = ({data}: PageProps<Queries.PortfolioQueryQuery>) => {
   const portfolio = data.contentfulPortfolio!
-  
+
   return (
     <Container>
       <Row>
@@ -22,82 +22,92 @@ const IndexPage = ({data}: PageProps<Queries.PortfolioQueryQuery>) => {
                   jobTitle={portfolio.details?.jobTitle ?? ""}
                   image={portfolio.details?.profileImage?.gatsbyImage}/>
 
-          <h2>Details</h2>
-          <p>
-            {portfolio.details?.location}<br/>
-            <a href={'tel:'+portfolio.details?.phone}>{portfolio.details?.phone}</a><br/>
-            <a href={'mailto:'+portfolio.details?.email}>{portfolio.details?.email}</a>
-          </p>
-          
-          <h2>Links</h2>
-          <p>
-            {portfolio.details?.links?.map((link) => (
-              <><a href={link?.url ?? ""}>{link?.name}</a><br/></>
+          <section className="mb-4">
+            <h2>Details</h2>
+            <p className="mb-0">{portfolio.details?.location}</p>
+            <p className="mb-0"><a href={'tel:'+portfolio.details?.phone}>{portfolio.details?.phone}</a></p>
+            <p className="mb-0"><a href={'mailto:'+portfolio.details?.email}>{portfolio.details?.email}</a></p>
+          </section>
+
+          <section className="mb-4">
+            <h2>Links</h2>
+            {portfolio.details?.links?.map((link, index) => (
+              <a key={index} href={link?.url ?? ""}>{link?.name}<br/></a>
             ))}
-          </p>
+          </section>
 
-          <h2>Languages</h2>
-          {portfolio.languages?.map((lang) => (
-            <Skill level={lang?.skillLevel ?? 0}>{lang?.language}</Skill>
-          ))}
+          <section className="mb-4">
+            <h2>Languages</h2>
+            {portfolio.languages?.map((lang) => (
+              <Skill level={lang?.skillLevel ?? 0}>{lang?.language}</Skill>
+            ))}
+          </section>
 
-          <h2>Skills</h2>
-          {portfolio.skills?.map((skill) => (
-            <Skill level={skill?.skillLevel ?? 0}>{skill?.name}</Skill>
-          ))}
+          <section>
+            <h2>Skills</h2>
+            {portfolio.skills?.map((skill) => (
+              <Skill level={skill?.skillLevel ?? 0}>{skill?.name}</Skill>
+            ))}
+          </section>
         </Col>
         <Col lg="9" sm="8" className="p-4 pb-2 p-sm-3 p-lg-4 p-xl-5">
-          {portfolio.sections?.map((section) => (
-            <>
+          {portfolio.sections?.map((section, index) => (
+            <section key={index}>
               <h2>{section?.title}</h2>
               {section?.content?.raw && documentToReactComponents(JSON.parse(section.content.raw))}
-            </>
+            </section>
           ))}
 
-          <h2>Employment History</h2>
-          {portfolio.employmentHistory?.map((job) => (
-            <>
-              <h3>{job?.jobTitle}, {job?.companyName?.companyName}</h3>
-              <h4>{formatDate(job?.startDate)} - {formatDate(job?.endDate)}</h4>
-              {job?.description?.raw && documentToReactComponents(JSON.parse(job.description.raw))}
-            </>
-          ))}
+          <section>
+            <h2>Employment History</h2>
+            {portfolio.employmentHistory?.map((job, index) => (
+              <div key={index}>
+                <h3>{job?.jobTitle}, {job?.companyName?.companyName}</h3>
+                <h4>{formatDate(job?.startDate)} - {formatDate(job?.endDate)}</h4>
+                {job?.description?.raw && documentToReactComponents(JSON.parse(job.description.raw))}
+              </div>
+            ))}
+          </section>
 
-          <h2>Education</h2>
-          {portfolio.education?.map((edu) => (
-            <>
-              <h3>{edu?.institutionName}</h3>
-              <h4>
-                {formatDate(edu?.startDate)}
-                {edu?.endDate && " - " + formatDate(edu?.endDate)}
-              </h4>
-              <p>
-                {edu?.description?.raw && documentToReactComponents(JSON.parse(edu.description.raw))}
-              </p>
-            </>
-          ))}
+          <section>
+            <h2>Education</h2>
+            {portfolio.education?.map((edu, index) => (
+              <div key={index}>
+                <h3>{edu?.institutionName}</h3>
+                <h4>
+                  {formatDate(edu?.startDate)}
+                  {edu?.endDate && " - " + formatDate(edu?.endDate)}
+                </h4>
+                <p>
+                  {edu?.description?.raw && documentToReactComponents(JSON.parse(edu.description.raw))}
+                </p>
+              </div>
+            ))}
+          </section>
         </Col>
       </Row>
       <Row>
         <Col lg="3" sm="4" className="p-4 pb-0 p-sm-3 p-lg-4 p-xl-5 pt-lg-0 pt-xl-0 bg-dark text-light">
-          <h2>Hobbies</h2>
-          <p>
-            {portfolio.hobbies?.map((hobby) => (
-              <>{hobby} <br/></>
+          <section className="mb-4">
+            <h2>Hobbies</h2>
+            {portfolio.hobbies?.map((hobby, index) => (
+              <p key={index} className="mb-0">{hobby}</p>
             ))}
-          </p>
+          </section>
         </Col>
         <Col lg="9" sm="8" className="p-4 p-sm-3 p-lg-4 p-xl-5 pt-lg-0 pt-xl-0">
-          <h2>Extra-curricular Activities</h2>
-          {portfolio.activities?.map((activity) => (
-            <>
-              <h3>{activity?.title}</h3>
-              <h4>{activity?.year}</h4>
-              <p>
-                {activity?.fullDescription?.raw && documentToReactComponents(JSON.parse(activity.fullDescription.raw))}
-              </p>
-            </>
-          ))}
+          <section>
+            <h2>Extra-curricular Activities</h2>
+            {portfolio.activities?.map((activity, index) => (
+              <div key={index}>
+                <h3>{activity?.title}</h3>
+                <h4>{activity?.year}</h4>
+                <p>
+                  {activity?.fullDescription?.raw && documentToReactComponents(JSON.parse(activity.fullDescription.raw))}
+                </p>
+              </div>
+            ))}
+          </section>
         </Col>
       </Row>
     </Container>
