f743055a66ba3e90e0200fe4206eee2d6a0a8bf9
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-08-25 15:09:37 +0100

    Add gatsby-plugin-preact to improve performance
    Integrated gatsby-plugin-preact and necessary dependencies in package.json and package-lock.json. Updated gatsby-config.ts to include the new plugin, aiming to improve performance and user experience.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,7 +12,7 @@ const config: GatsbyConfig = {
     // If you use VSCode you can also use the GraphQL plugin
     // Learn more at: https://gatsby.dev/graphql-typegen
     graphqlTypegen: true,
-    plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-preact", {
         resolve: `gatsby-plugin-purgecss`,
         options: {
             printRejected: true, // Print removed selectors and processed file names
